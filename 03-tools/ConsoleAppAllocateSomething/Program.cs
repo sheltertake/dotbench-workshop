8d62f05187a3a0972a43e8a7102b75426419e48f
Massimiliano Balestrieri <Massimiliano.Balestrieri@deltatre.com>
2019-09-16 12:29:24 +0200

    wip

---
+++ 03-tools/ConsoleAppAllocateSomething/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Linq;
+
+namespace ConsoleAppAllocateSomething
+{
+    class MyType
+    {
+        public Guid Guid = Guid.NewGuid();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            var myTypes = new MyType[1_000_000];
+            for (var x = 0; x < 1_000_000; x++)
+            {
+                myTypes[x] = new MyType();
+            }
+            Console.WriteLine(myTypes.Count());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03-tools/ConsoleAppCpu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppCpu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IsPrime(2147483647);
+        }
+
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= n - 1; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
